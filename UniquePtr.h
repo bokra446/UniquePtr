d870e11836066fc34f1ad47b93ed6472490a78a4
bokra <katja44667@gmail.com>
2021-05-25 08:04:21 +0300

    add template

--- UniquePtr.h
+++ UniquePtr.h
@@ -1,24 +1,45 @@
 #pragma once
-
-using ValueType = char;
-
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+template <typename T>
 class UniquePtr {
+    using ValueType = T;
 private:
     ValueType* _ptr;
 public:
+
     // Конструктор
-    UniquePtr();
+    UniquePtr() {
+        _ptr = nullptr;
+    }
     // Конструктор из указателя
-    explicit UniquePtr(ValueType* ptr);
+    explicit UniquePtr(ValueType* ptr) {
+        _ptr = ptr;
+    }
 
     // Конструктор перемещения
-    UniquePtr(UniquePtr&& o);
+    UniquePtr(UniquePtr&& o) {
+        this->_ptr = o._ptr;
+        o._ptr = nullptr;
+        std::cout << "move construct \n";
+    }
 
     // Конструктор копирования
     UniquePtr(const UniquePtr& o) = delete;
 
     // оператор присваивания через перемещение
-    UniquePtr& operator=(UniquePtr&& o);
+    UniquePtr& operator=(UniquePtr&& o) {
+        std::cout << "move operation \n";
+        if (o._ptr == _ptr)
+            return *this;
+        if (_ptr != nullptr) {
+            delete[] _ptr;
+        }
+        _ptr = nullptr;
+        std::swap(_ptr, o._ptr);
+        return *this;
+    }
         // o == this ?
 
         // на этот момент тут уже что-то может быть
@@ -29,30 +50,55 @@ public:
 
     // оператор присваивания - присвоить новый указатель
     // Не забывать про старую память
-    UniquePtr& operator=(ValueType* ptr);
+    UniquePtr& operator=(ValueType* ptr) {
+        std::cout << " operator = \n";
+        if (this != nullptr) {
+            delete[] _ptr;
+        }
+        _ptr = ptr;
+        return *this;
+    }
 
     // сбрасывает _ptr в nullptr
     // не забыть почистить память память память!
-    void reset();
+    void reset() {
+        reset(nullptr);
+    }
 
     // reset + присвоение нового указателя
-    void reset(ValueType* ptr);
+    void reset(ValueType* ptr) {
+        std::cout << "this is empty " << static_cast<bool>(this) << std::endl;
+        if (this != nullptr) {
+            delete[] _ptr;
+        }
+        _ptr = ptr;
+    }
 
     // "Освобождение" указателя
     // без удаления памяти
-    void release();
+    void release() {
+        _ptr = nullptr;
+    }
 
     // возвращает, внутри nullptr или нет
-    operator bool() const;
+    operator bool() const {
+        return _ptr;
+    }
 
     // разыменовывание
-    ValueType& operator*() const;
+    ValueType& operator*() const {
+        return *_ptr;
+    }
 
     // оператор стрелочки
-    ValueType* operator->() const;
+    ValueType* operator->() const {
+        return get();
+    }
 
     // возвращает указатель
-    ValueType* get() const;
+    ValueType* get() const {
+        return _ptr;
+    }
 
 
 };
